12200ec6d229e851b60f77dbef36e55cdeb691cf
Nina Weng <nina.weng@foxmail.com>
2023-03-14 09:29:59 +0100

    -

--- data_preprocess/preprocess.py
+++ data_preprocess/preprocess.py
@@ -9,31 +9,36 @@ import numpy as np
 
 FOLDER_SPECIFIC='ISIC_2019_' # or ''
 
-# point to the parent directory that contains the folder 'isic_images'
-# img_data_dir = '/work3/ninwe/dataset/isic/'
-img_data_dir = 'D:\\ninavv\\phd\\data\\isic\\'
-csv_file = '../datafiles/'+FOLDER_SPECIFIC+'metadata-clean-split.csv'
+def main():
 
-df = pd.read_csv(csv_file,header=0)
+    # point to the parent directory that contains the folder 'isic_images'
+    # img_data_dir = '/work3/ninwe/dataset/isic/'
+    img_data_dir = 'D:\\ninavv\\phd\\data\\isic\\'
+    csv_file = '../datafiles/'+FOLDER_SPECIFIC+'metadata-clean-split.csv'
 
-df['path_preproc'] = df['path']
+    df = pd.read_csv(csv_file,header=0)
 
-preproc_dir = FOLDER_SPECIFIC+'preproc_224x224/'
-out_dir = img_data_dir
+    df['path_preproc'] = df['path']
 
-if not os.path.exists(out_dir + preproc_dir):
-    os.makedirs(out_dir + preproc_dir)
+    preproc_dir = FOLDER_SPECIFIC+'preproc_224x224/'
+    out_dir = img_data_dir
 
-for idx, p in enumerate(tqdm(df['path'])):
+    if not os.path.exists(out_dir + preproc_dir):
+        os.makedirs(out_dir + preproc_dir)
 
-    split =  p.split("/")
-    preproc_filename = split[-1]
-    df.loc[idx, 'path_preproc'] = preproc_dir + preproc_filename
-    out_path = out_dir + preproc_dir + preproc_filename
+    for idx, p in enumerate(tqdm(df['path'])):
 
-    if not os.path.exists(out_path):
-        image = imread(img_data_dir + p)
-        image = resize(image, output_shape=(224, 224), preserve_range=True)
-        imsave(out_path, image.astype(np.uint8))
+        split =  p.split("/")
+        preproc_filename = split[-1]
+        df.loc[idx, 'path_preproc'] = preproc_dir + preproc_filename
+        out_path = out_dir + preproc_dir + preproc_filename
 
-df.to_csv(csv_file)
+        if not os.path.exists(out_path):
+            image = imread(img_data_dir + p)
+            image = resize(image, output_shape=(224, 224), preserve_range=True)
+            imsave(out_path, image.astype(np.uint8))
+
+    df.to_csv(csv_file)
+
+if __name__ == '__main__':
+    main()
