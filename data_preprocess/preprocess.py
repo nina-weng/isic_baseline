2844c35753800ac4a96203ae17a6426126769da2
Nina Weng <nina.weng@foxmail.com>
2023-03-21 12:02:59 +0100

    -

--- data_preprocess/preprocess.py
+++ data_preprocess/preprocess.py
@@ -18,7 +18,7 @@ def main():
 
     df = pd.read_csv(csv_file,header=0)
 
-    df['path_preproc'] = df['path']
+    #df['path_preproc'] = df['path']
 
     preproc_dir = FOLDER_SPECIFIC+'preproc_224x224/'
     out_dir = img_data_dir
@@ -30,7 +30,7 @@ def main():
 
         split =  p.split("/")
         preproc_filename = split[-1]
-        df.loc[idx, 'path_preproc'] = preproc_dir + preproc_filename
+        #df.loc[idx, 'path_preproc'] = preproc_dir + preproc_filename
         out_path = out_dir + preproc_dir + preproc_filename
 
         if not os.path.exists(out_path):
@@ -38,7 +38,23 @@ def main():
             image = resize(image, output_shape=(224, 224), preserve_range=True)
             imsave(out_path, image.astype(np.uint8))
 
+    #df.to_csv(csv_file)
+
+
+def get_preproc_path():
+    # img_data_dir = '/work3/ninwe/dataset/isic/'
+    img_data_dir = 'D:\\ninavv\\phd\\data\\isic\\'
+    csv_file = '../datafiles/' + FOLDER_SPECIFIC + 'metadata-clean-split.csv'
+    df = pd.read_csv(csv_file, header=0)
+    df['path_preproc'] = df['path']
+
+    preproc_dir = FOLDER_SPECIFIC + 'preproc_224x224/'
+    for idx, p in enumerate(tqdm(df['path'])):
+        split = p.split("/")
+        preproc_filename = split[-1]
+        df.loc[idx, 'path_preproc'] = preproc_dir + preproc_filename
     df.to_csv(csv_file)
 
 if __name__ == '__main__':
-    main()
+    # main()
+    get_preproc_path()
